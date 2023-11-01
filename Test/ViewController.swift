6c8647590a0c3397a164ff086c2ff74497bb19fa
Matthew <juehua.lu@7eleven.com.au>
2023-11-01 22:40:20 +1100

    bonus question

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -1,16 +1,17 @@
-/*Question 2:
+/*
+Question 2:
 
- Use `URLSession` to initiate a API request to the following endpoint:
-
- https://mocki.io/v1/ee063c21-d08d-4cdc-98d0-7ad45e3bd9c6
-
-Show the highest fuel price in the detail label.
- 
-Leverage `UIStackView` to construct the interface as depicted in the screenshot.
+Use `UIStackView` to construct the interface as depicted in the screenshot.
 
 Place the title label and image view at the top, with the detail label positioned beneath the title label.
  
 
+Bonus question:
+ Use `URLSession` to initiate a API request to the following endpoint:
+
+ https://mocki.io/v1/ee063c21-d08d-4cdc-98d0-7ad45e3bd9c6
+
+ Show the highest fuel price in the detail label.
 */
 
 
@@ -49,21 +50,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Call API here
-        API().fetchFuelPrice { maxPrice in
-            
-        }
-        
         // Setup UI here
         NSLayoutConstraint.activate([
           
         ])
+        
+        
+        /* Bonus question
+        // Call API here
+         API().fetchFuelPrice { maxPrice in
+            
+         }
+         */
     }
 
 }
 
 
-
+/* Bonus question*/
 class API {
     func fetchFuelPrice(completion: @escaping (Int?) -> Void) {
         
